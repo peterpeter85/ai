c00c51057e40bd38e7cacef9e779cef740c190f5
peterpeter85 <160134918+peterpeter85@users.noreply.github.com>
2024-02-20 22:04:12 +0900

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -3,9 +3,6 @@ from streamlit_chat import message
 import qrcode
 import textwrap
 import time
-with st.sidebar:
-  st.header("hello")
-  u=st.text_input("hello")
 message("hello!", is_user=True)
 time.sleep(5)
 message("hello!")
