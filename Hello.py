2185ea076979b2556c59f78cd5679632ca62416d
peterpeter85 <160134918+peterpeter85@users.noreply.github.com>
2024-02-20 22:30:25 +0900

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -12,7 +12,7 @@ with st.form("you:", clear_on_submit=True):
   submitted=st.form_submit_button("✅")
 if submitted and user:
   st.session_state.past.append(user)
-  message(st.session_state.past[index],is_user=True)
+  message(user,is_user=True)
   message(user,is_user=True)
   if user=="너는 누구야?":
     st.session_state.ai.append("저는 인공지능 챗봇 챗챗입니다.")
