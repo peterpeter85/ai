7357a199ada3deafa251186e1adc180233152c0e
peterpeter85 <160134918+peterpeter85@users.noreply.github.com>
2024-02-22 22:35:28 +0900

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -8,7 +8,7 @@ from PIL import Image
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 im=Image.open("large.png")
 st.sidebar.title("Our logo")
-st.sidebar.image(im, width=350, caption="chatchat's logo")
+st.sidebar.image(im, width=250, caption="chatchat's logo")
 if "ai" not in st.session_state:
   st.session_state["ai"]=[]
 with st.form("you:", clear_on_submit=True):
